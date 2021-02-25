cf1f9608c0778a36072ef134b5326f0ee9d30e35
despar <despar@microsoft.com>
2021-02-24 18:39:37 -0800

    Assignment complete

--- DojoSurvey/DojoSurvey_app/views.py
+++ DojoSurvey/DojoSurvey_app/views.py
@@ -7,8 +7,8 @@ def result(request):
     if request.method == 'POST':
         context = {
             'name': request.POST['name'],
-            'lang': request.POST['location'],
-            'loc': request.POST['language']
+            'loc': request.POST['location'],
+            'lang': request.POST['language']
         }
         return render(request, 'result.html', context)
     return render(request, 'result.html')
